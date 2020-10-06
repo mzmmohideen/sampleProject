61e82f03b57c303bf3e32e4440121a3d4b1b1175
mzmmohideen <mzmmohideen@gmail.com>
2020-10-06 12:42:27 +0530

    Second Commit

--- sample.py
+++ sample.py
@@ -2,6 +2,5 @@
 # @Author: mzmmohideen
 # @Date:   2020-10-06 12:32:53
 # @Last Modified by:   mzmmohideen
-# @Last Modified time: 2020-10-06 12:37:48
+# @Last Modified time: 2020-10-06 12:39:15
 print("Hello World!")
-print("Hi Hello")
