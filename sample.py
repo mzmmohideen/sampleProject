47fbff95e74ed027968d32d881891ccd8eac89fc
mzmmohideen <mzmmohideen@gmail.com>
2020-10-06 12:38:48 +0530

    Created Sample File

---
+++ sample.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: mzmmohideen
+# @Date:   2020-10-06 12:32:53
+# @Last Modified by:   mzmmohideen
+# @Last Modified time: 2020-10-06 12:37:48
+print("Hello World!")
+print("Hi Hello")
